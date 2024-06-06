c06bef8126e6efb636d23c123688af8cc9127777
prodiegus <diego.d.a.f.v.2000@gmail.com>
2024-06-06 19:48:26 -0400

    Nav bar entre componentes

--- app/src/main/java/utalca/gestor_qr/MainActivity.kt
+++ app/src/main/java/utalca/gestor_qr/MainActivity.kt
@@ -1,20 +1,61 @@
 package utalca.gestor_qr
 
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import utalca.gestor_qr.MainViews.Crear
+import utalca.gestor_qr.MainViews.Escanear
+import utalca.gestor_qr.MainViews.Historial
 
 class MainActivity : AppCompatActivity() {
+
+    val escanear = Escanear()
+    val crear = Crear()
+    val historial = Historial()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        val navigationView = findViewById<BottomNavigationView>(R.id.navigation)
+        navigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        loadFragment(escanear)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.navigation), { view, insets ->
+            val params = view.layoutParams as RelativeLayout.LayoutParams
+            params.topMargin = insets.getInsets(WindowInsetsCompat.Type.displayCutout()).top
+            view.layoutParams = params
             insets
-        }
+        })
     }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.escanear -> {
+                loadFragment(escanear)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.crear -> {
+                loadFragment(crear)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.historial -> {
+                loadFragment(historial)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+     fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_container, fragment)
+         transaction.commit()
+     }
 }
+
+
