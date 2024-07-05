230efc29bff7e770ff985a93dc654cfa664db587
Juan Farias <juanjo.farias.90@gmail.com>
2024-07-04 23:35:57 -0400

    creacion de qr

--- app/src/main/java/utalca/gestor_qr/MainViews/Crear.kt
+++ app/src/main/java/utalca/gestor_qr/MainViews/Crear.kt
@@ -5,56 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import utalca.gestor_qr.MainModel.QR_Generator
 import utalca.gestor_qr.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Crear.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Crear : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var tituloEditText: EditText
+    private lateinit var enlaceEditText: EditText
+    private lateinit var crearButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_crear, container, false)
+        val view = inflater.inflate(R.layout.fragment_crear, container, false)
+
+        tituloEditText = view.findViewById(R.id.titulo_edit_text)
+        enlaceEditText = view.findViewById(R.id.enlace_edit_text)
+        crearButton = view.findViewById(R.id.crear_button)
+
+        crearButton.setOnClickListener {
+            val titulo = tituloEditText.text.toString()
+            val enlace = enlaceEditText.text.toString()
+            mostrarPrevisualizacion(titulo, enlace)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Crear.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Crear().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun mostrarPrevisualizacion(titulo: String, enlace: String) {
+        val builder = AlertDialog.Builder(requireContext())
+        val view = layoutInflater.inflate(R.layout.dialog_previsualizacion, null)
+
+        val qrImageView = view.findViewById<ImageView>(R.id.qr_image_view)
+        val tituloTextView = view.findViewById<TextView>(R.id.titulo_text_view)
+        val enlaceTextView = view.findViewById<TextView>(R.id.enlace_text_view)
+
+        val qrBitmap = QR_Generator().generateQRCode(enlace, requireContext())
+        qrImageView.setImageBitmap(qrBitmap)
+        tituloTextView.text = titulo
+        enlaceTextView.text = enlace
+
+        builder.setView(view)
+        builder.setPositiveButton("Aceptar", null)
+        builder.setNegativeButton("Cancelar", null)
+        builder.show()
     }
 }
